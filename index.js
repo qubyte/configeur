60a677b667fa33dd6a43160367423a69eb566324
Mark S. Everitt <mark.s.everitt@gmail.com>
2017-01-16 10:34:56 +0000

    Adds a TypeScript definition file.

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ const makeParsers = require('./lib/makeParsers');
 module.exports = function configeur(schema, options = {}) {
   return processConfig(schema, process.env, makeParsers(options.parsers));
 };
+
+module.exports.default = module.exports;
